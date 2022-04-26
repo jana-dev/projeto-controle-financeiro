6800d2f4d6af541bf71f83cca0b3348145ed12c3
Janaina Tavares <janainaktavares@outlook.com>
2022-04-25 23:51:20 -0300

    Iniciado o armazenamento dinamico da tabela

--- main.js
+++ main.js
@@ -27,6 +27,61 @@ function validarValor(){
   } 
 }
 
+// função para armazenar o conteudo do extrato dinamicamente na tabela, ainda em andamento
+
+function desenhaTabela(){
+
+  var extrato = [
+    {
+      tipo: 'Compra',
+      nome: 'Bicicleta',
+      valor: 'R$ 2.500,00'
+    },
+    {
+      tipo: 'Compra',
+      nome: 'Moto',
+      valor: 'R$ 30.000,00'
+    },
+    {
+      tipo: 'Venda',
+      nome: 'Cama',
+      valor: 'R$ 1.500,00'
+    }
+  ];
+
+
+  for (transacao in extrato){
+      document.querySelector('table.tabela tbody').innerHTML += 
+        `<tr class="conteudo-dinamico">
+        <td class="espaco-simbolo">
+        ${extrato[transacao].tipo ? '+' : '-'}
+        </td>
+        <td class="conteudo-tabela alinhamento-esquerdo">
+          ${extrato[transacao].nome}
+        </td>
+        <td class="alinhamento-direito">
+          ${extrato[transacao].valor}
+        </td>
+      </tr>
+      <tr class="linha-simples-separadora conteudo-dinamico"> <!--linha apenas para desenhar uma borda-->
+        <td colspan="3"></td>
+      </tr>`
+      
+  }
+
+}
+
+desenhaTabela();
+
+
+
+
+
+
+
+
+
+
 
 
 
