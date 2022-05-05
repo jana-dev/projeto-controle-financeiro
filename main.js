c2554498c2baa45d6e534d1327ca71c5ced500ea
Janaina Tavares <janainaktavares@outlook.com>
2022-05-05 19:26:59 -0300

    Ajustado o input valor para nao permitir que o usuario possa colar valores que nao sao numericos e retirado o reload da pagina apos submeter, porem agora nao esta apagando as informacoes do form depois de submeter, ainda preciso validar isso

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-/*Função para abrir e fechar o menu mobile*/
 var size = screen.width;
+var transacao = [];
+var total = 0;
+
+desenhaTabela();
+
+/*Função para abrir e fechar o menu mobile*/
 function fecharMenu() {
   document.getElementById("menu-conteiner").style.display = "none";
 }
@@ -19,7 +24,8 @@ $(document).ready(function () {
 });
 
 /* Validação do campo valor*/
-function validarValor() {
+function validarValor(e) {
+  e.preventDefault();
   var validacao = document.getElementById("valor").value;
   if (validacao == "") {
     alert("Digite um valor");
@@ -27,7 +33,6 @@ function validarValor() {
 }
 
 // Armazena no LocalStorage
-var transacao = [];
 function cadastrar(e) {
   e.preventDefault();
   console.log("clicou no adicionar transacao");
@@ -45,19 +50,16 @@ function cadastrar(e) {
   });
 
   localStorage.setItem("transacao", JSON.stringify(transacao));
-  console.log({ transacao });
   desenhaTabela();
-  location.reload();
 }
 
 // Desenha as linhas das transações na tabela pegando a chave do localStorage
 function desenhaTabela() {
-
-  transacao = JSON.parse(localStorage.getItem('transacao'));
-  if(transacao != null){
-    document.querySelector("#dinamico").innerHTML = transacao.map((item) => {
-      return(
-        `<tr class="conteudo-dinamico">
+  transacao = JSON.parse(localStorage.getItem("transacao"));
+  if (transacao != null) {
+    document.querySelector("#dinamico").innerHTML = transacao
+      .map((item) => {
+        return `<tr class="conteudo-dinamico">
         <td class="espaco-simbolo">
         ${item.tipo == "venda" ? "+" : "-"}
         </td>
@@ -70,94 +72,90 @@ function desenhaTabela() {
       </tr>
       <tr class="linha-simples-separadora conteudo-dinamico"> 
         <td colspan="3"></td>
-      </tr>`
-      )
-    }).join('')
-    listarTotal()
-    if(transacao != null){
-      document.getElementById('espaco-linhadupla').style.display = "none";
-      document.getElementById('nenhumaCadastrada').style.display = "none";
+      </tr>`;
+      })
+      .join("");
+    listarTotal();
+    if (transacao != null) {
+      document.getElementById("espaco-linhadupla").style.display = "none";
+      document.getElementById("nenhumaCadastrada").style.display = "none";
     }
   }
 }
 
-desenhaTabela()
-
-
 // função para deletar todas as transações do localstorage
-function deletarTransacoes(){
-  confirmar = confirm("Tem certeza que deseja excluir todos os registros armazenados?")
-  if(confirmar == true){
-    localStorage.clear('transacao')
-    alert("Registros excluídos")
-    location.reload()
-  }else{
-    alert("Registros mantidos")
+function deletarTransacoes() {
+  confirmar = confirm(
+    "Tem certeza que deseja excluir todos os registros armazenados?"
+  );
+  if (confirmar == true) {
+    localStorage.clear("transacao");
+    alert("Registros excluídos");
+    location.reload();
+  } else {
+    alert("Registros mantidos");
   }
-  desenhaTabela()
+  desenhaTabela();
 }
-
 
 // função que realiza o cálculo dos valores inseridos no form
 
-var total = 0
 function calculoValores() {
-    let transacao = JSON.parse(localStorage.getItem('transacao'))
-    let totalStrVenda = []
-    let totalStrCompra = []
-    let totalNbrVenda = []
-    let totalNbrCompra = []
-    let totalVenda = 0
-    let totalCompra = 0
-    let i = 0
-    let j = 0
-    if (transacao != null) {
-        for (; i < transacao.length; i++) {
-            if (transacao[i].tipo == "compra") {
-                totalStrCompra = [transacao[i].valor.replace(/\D/g, '')]
-                totalNbrCompra = Number.parseFloat(totalStrCompra)
-                totalCompra += totalNbrCompra
-            }
-        }
-        for (; j < transacao.length; j++) {
-            if (transacao[j].tipo == "venda") {
-                totalStrVenda = [transacao[j].valor.replace(/\D/g, '')]
-                totalNbrVenda = Number.parseFloat(totalStrVenda)
-                totalVenda += totalNbrVenda
-            }
-        }
-        total = totalVenda - totalCompra
+  let transacao = JSON.parse(localStorage.getItem("transacao"));
+  let totalStrVenda = [];
+  let totalStrCompra = [];
+  let totalNbrVenda = [];
+  let totalNbrCompra = [];
+  let totalVenda = 0;
+  let totalCompra = 0;
+  let i = 0;
+  let j = 0;
+  if (transacao != null) {
+    for (; i < transacao.length; i++) {
+      if (transacao[i].tipo == "compra") {
+        totalStrCompra = [transacao[i].valor.replace(/\D/g, "")];
+        totalNbrCompra = Number.parseFloat(totalStrCompra);
+        totalCompra += totalNbrCompra;
+      }
     }
+    for (; j < transacao.length; j++) {
+      if (transacao[j].tipo == "venda") {
+        totalStrVenda = [transacao[j].valor.replace(/\D/g, "")];
+        totalNbrVenda = Number.parseFloat(totalStrVenda);
+        totalVenda += totalNbrVenda;
+      }
+    }
+    total = totalVenda - totalCompra;
+  }
 }
 
 /*Função que lista na tela se o valor apresentado é de lucro ou de prejuízo, baseado no valor da variável 'total'*/
 function listarTotal() {
-  calculoValores()
-  formatarMoeda()
-  var campoTotal = document.getElementById('resultTotal')
+  calculoValores();
+  formatarMoeda();
+  var campoTotal = document.getElementById("resultTotal");
   resultTotal.innerHTML = "R$ " + totalFormatado;
 
   if (total > 0) {
-      lucro.innerHTML = "[LUCRO]"
-  }
-  else if (total < 0) {
-      lucro.innerHTML = "[PREJUIZO]"
-  }
-  else {
-      lucro.innerHTML = ""
+    lucro.innerHTML = "[LUCRO]";
+  } else if (total < 0) {
+    lucro.innerHTML = "[PREJUIZO]";
+  } else {
+    lucro.innerHTML = "";
   }
 }
 
 function formatarMoeda() {
-  totalFormatado = total
-  totalFormatado = totalFormatado + '';
-  totalFormatado = parseInt(totalFormatado.replace(/[\D]+/g, ''));
-  totalFormatado = totalFormatado + '';
+  totalFormatado = total;
+  totalFormatado = totalFormatado + "";
+  totalFormatado = parseInt(totalFormatado.replace(/[\D]+/g, ""));
+  totalFormatado = totalFormatado + "";
   totalFormatado = totalFormatado.replace(/([0-9]{2})$/g, ",$1");
 
   if (totalFormatado.length > 6) {
-      totalFormatado = totalFormatado.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+    totalFormatado = totalFormatado.replace(
+      /([0-9]{3}),([0-9]{2}$)/g,
+      ".$1,$2"
+    );
   }
 }
-
-
